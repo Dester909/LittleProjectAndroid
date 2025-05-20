8a5ed354c7b504cfdddc6f3b1a7e60c315c5904d
Dester909 <marco515.ags@hotmail.com>
2025-05-19 22:32:03 -0600

    Tareas en segundo plano added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,9 @@ android {
 
 
 dependencies {
+    implementation ("androidx.biometric:biometric:1.2.0-alpha05")
+    implementation("androidx.work:work-runtime-ktx:2.10.1")
+    implementation ("androidx.work:work-runtime:2.10.1")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -70,20 +73,20 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
     implementation(libs.coil.compose)
 
     val room_version = "2.6.1"
 
     implementation("androidx.room:room-runtime:$room_version")
-    implementation("androidx.room:room-ktx:$room_version") // opcional pero recomendado
+    implementation("androidx.room:room-ktx:$room_version")
     ksp("androidx.room:room-compiler:$room_version")
 
-    // retrofit
+    // Retrofit
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 }
